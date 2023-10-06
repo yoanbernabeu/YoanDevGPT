7ee29c978824534f8f350fa46d094a327e74ff3f
Pascal Canadas <pascal@mirandais.ovh>
2023-10-06 18:19:03 +0200

    📦 FEATURE: Ajout de la fonctionnalité de chargement de listes de prompts personnalisées pour ChatGPT

--- chrome/src/background.js
+++ chrome/src/background.js
@@ -1,41 +1,78 @@
-const DATA_URL = "https://raw.githubusercontent.com/yoanbernabeu/YoanDevGPT/main/data.json";
+const DEFAULT_DATA_URL = "https://raw.githubusercontent.com/yoanbernabeu/YoanDevGPT/main/data.json";
 
-chrome.runtime.onInstalled.addListener(() => {
-    fetch(DATA_URL)
-        .then((response) => response.json())
-        .then((data) => {
-            data.forEach((item, index) => {
-                chrome.contextMenus.create({
-                    id: index.toString(),
-                    title: item.title,
-                    contexts: ["editable"],
-                    parentId: "parent",
+chrome.runtime.onInstalled.addListener(setup);
+chrome.runtime.onStartup.addListener(setup);
+
+function setup() {
+    // clear all context menus entries firstly
+    chrome.contextMenus.removeAll(function() {
+        chrome.storage.local.get('dataURL', data => {
+            let DATA_URL = data.dataURL || DEFAULT_DATA_URL;
+
+            fetch(DATA_URL)
+                .then((response) => response.json())
+                .then((data) => {
+                    data.forEach((item, index) => {
+                        chrome.contextMenus.create({
+                            id: index.toString(),
+                            title: item.title,
+                            contexts: ["editable"],
+                            parentId: "parent",
+                        });
+                    });
                 });
+
+            chrome.contextMenus.create({
+                id: "parent",
+                title: "YoanDevGPT",
+                contexts: ["editable"],
+            });
+
+            // Create a context menu to reset URL to default
+            chrome.contextMenus.create({
+                id: "reset",
+                title: "Reset URL to default",
+                contexts: ["browser_action"],
             });
         });
-
-    chrome.contextMenus.create({
-        id: "parent",
-        title: "YoanDevGPT",
-        contexts: ["editable"],
     });
-});
+}
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-    const index = parseInt(info.menuItemId);
+    chrome.storage.local.get('dataURL', data => {
+        let DATA_URL = data.dataURL || DEFAULT_DATA_URL;
 
-    fetch(DATA_URL)
-        .then((response) => response.json())
-        .then((data) => {
-            const prompt = data[index].prompt;
-            chrome.scripting.executeScript({
-                target: { tabId: tab.id },
-                function: setInputField,
-                args: [prompt],
-            });
-        });
+        console.log(`URL de données actuelle : ${DATA_URL}`);
+
+        if (info.menuItemId === "reset") {
+            DATA_URL = DEFAULT_DATA_URL;
+            chrome.storage.local.set({ dataURL: DATA_URL });
+        } else {
+            const index = parseInt(info.menuItemId);
+
+            fetch(DATA_URL)
+                .then((response) => response.json())
+                .then((data) => {
+                    const prompt = data[index].prompt;
+                    chrome.scripting.executeScript({
+                        target: { tabId: tab.id },
+                        function: setInputField,
+                        args: [prompt],
+                    });
+                });
+        }
+    });
 });
 
 function setInputField(prompt) {
     document.activeElement.value = prompt;
 }
+
+chrome.storage.onChanged.addListener(function(changes) {
+    for (let key in changes) {
+        if (key === 'dataURL') {
+            setup();
+            break;
+        }
+    }
+});
